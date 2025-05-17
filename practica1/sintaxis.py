66b8914f8864aa813f454e3ccdaeb953ec366824
Samael Sánchez <antonio.sanchez35@hotmail.com>
2025-05-16 18:21:09 -0600

    Variables en python

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -8,5 +8,24 @@
 """
 
 # 2. Strings
-print("Hola soy una cadena 🦍")
-print("yo soy otra cadena")
+""" print("Hola soy una cadena 🦍")
+print("yo soy otra cadena") """
+
+cadena = "hola, soy una cadena"
+print(len(cadena))
+print(cadena[2:5])
+print(cadena[2:])
+print(cadena[:5])
+
+# 3. Variables
+# Nunca poner un numero al inicio
+# ESTO ESTA MAL ---> 234dk = "adios"
+# investigar zip
+
+x = "samael"
+x = int(3)
+y = float(3)
+z = str(3)
+print(x, y, z)
+print(type(x))
+
