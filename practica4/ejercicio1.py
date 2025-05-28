b03c10a14ec7dc374ce9bf156e5d5592f8154399
Samael Sánchez <antonio.sanchez35@hotmail.com>
2025-05-28 15:32:55 -0600

    Comentarios añadidos

--- practica4/ejercicio1.py
+++ practica4/ejercicio1.py
@@ -1,10 +1,17 @@
+# Función que retorna la lista con todos los impares
 def impares(n):
+    # validar que el número es mayor a 10
     if not n > 10:
         raise ValueError("El número debe ser mayor a 10")
+    
+    # se crea la lista, y recorremos todos los números hasta n
+    # si son impares, se añaden a la lista
     lista = []
     for i in range(2, n + 1):
         if i % 2 != 0:
             lista.append(i)
+    
+    # finalmente retornamos la lista
     return lista
 
 
